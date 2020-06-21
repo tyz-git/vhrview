5a3f98d4358d1f944b267c4191b43665d48aab26
tongyazhou <yazhou5969@163.com>
2020-06-21 23:35:28 +0800

    操作员

--- src/components/deptManagement.vue
+++ src/components/deptManagement.vue
@@ -2,7 +2,7 @@
     <div>
         <el-input
                 class="searchInputClass"
-                placeholder="输入部门名称..."
+                placeholder="请输入要查询部门名称..."
                 v-model="filterText">
         </el-input>
 
@@ -11,13 +11,53 @@
                 :data="departmentList"
                 :props="defaultProps"
                 :filter-node-method="filterNode"
+                :expand-on-click-node="false"
                 ref="tree">
+        <!--node:当前元素，data:当前元素中的数据(Json对象) -->
+        <span class="custom-tree-node" slot-scope="{ node, data }" style="display: flex;justify-content:space-between;width: 100%;">
+            <span>{{ data.name }}</span>  <!-- 和:props="defaultProps" 中的label属性是一个作用。 -->
+            <span>
+              <el-button
+                      type="primary"
+                      size="mini" class="butCls"
+                      @click="() => showAddDialog(data)">
+                添加部门
+              </el-button>
+              <el-button
+                      type="danger"
+                      size="mini" class="butCls"
+                      @click="() => deleteDept(data)">
+                删除部门
+              </el-button>
+            </span>
+        </span>
         </el-tree>
+        <div>
+            <el-dialog
+                    title="添加部门"
+                    :visible.sync="dialogVisible"
+                    width="30%">
+            <table>
+                <tr>
+                    <td><el-tag style="width: 70px;">父节点</el-tag></td>
+                    <td><el-input v-model="pname" disabled></el-input></td>
+                </tr>
+                <tr>
+                    <td><el-tag style="width: 70px;">部门名称</el-tag></td>
+                    <td><el-input placeholder="请输入部门名称..." v-model="addDeptModel.name" @keyup.enter.native="addDept"></el-input></td>
+                </tr>
+            </table>
+            <span slot="footer" class="dialog-footer">
+                <el-button type="primary" size="small" @click="addDept">确 定</el-button>
+                <el-button size="small" @click="dialogVisible = false">取 消</el-button>
+            </span>
+            </el-dialog>
+        </div>
     </div>
 </template>
 
 <script>
-    import {getRequest} from "../utils/api";
+    import {deleteRequest, getRequest, postRequest} from "../utils/api";
 
     export default {
         name: "deptManagement",
@@ -36,6 +76,62 @@
         },
 
         methods: {
+            deleteAfterInitDept(depts, id) {
+                for(let i = 0; i < depts.length; i++) {
+                    let dept = depts[i];
+                    if (dept.id == id){
+                        //删除操作，从i位置开始，删除1个元素
+                        depts.splice(i,1);
+                    }else{
+                        this.deleteAfterInitDept(dept.children, id);
+                    }
+                }
+            },
+            deleteDept(data){
+                if (data.parent){
+                    this.$message.error("此项为父部门");
+                }else{
+                    this.$confirm('此操作将永久删除'+ data.name +', 是否继续?', '提示', {
+                        confirmButtonText: '确定',
+                        cancelButtonText: '取消',
+                        type: 'warning'
+                    }).then(() => {
+                        deleteRequest("/system/basic/department/" + data.id).then(resp=>{
+                            if (resp){
+                                this.deleteAfterInitDept(this.departmentList, resp.object);
+                            }
+                        })
+                    }).catch(() => {
+                        this.$message({
+                            type: 'info',
+                            message: '已取消删除'
+                        });
+                    });
+                }
+            },
+            addAfterInitDept(depts, department){
+                depts.forEach(dept=>{
+                    if (dept.id == department.parentId){
+                        dept.children = dept.children.concat(department);
+                    }else{
+                        this.addAfterInitDept(dept.children, department);
+                    }
+                })
+            },
+            addDept(){
+                postRequest("/system/basic/department/",this.addDeptModel).then(resp=>{
+                    if (resp){
+                        this.dialogVisible = false;
+                        this.addAfterInitDept(this.departmentList, resp.object);
+                        this.addDeptModel.name = '';
+                    }
+                })
+            },
+            showAddDialog(data) {
+                this.dialogVisible = true;
+                this.pname = data.name;
+                this.addDeptModel.parentId = data.id;
+            },
             //value:filterText变量的新值。data:树中每个节点的数据(json对象),一个data对应着一个json对象
             filterNode(value, data) {
                 //返回true就是把当前节点留下来，返回false就是把当前节点去掉
@@ -53,6 +149,12 @@
 
         data() {
             return {
+                addDeptModel: {
+                  parentId: -1,
+                  name: ''
+                },
+                pname: '',
+                dialogVisible: false,
                 filterText: '',
                 defaultProps: {
                     children: 'children',
@@ -70,6 +172,9 @@
         display: flex;
         justify-content: flex-start;
         margin-bottom: 15px;
+    }
+    .butCls{
+        padding: 2px;
     }
 
 </style>
--- src/views/sys/SysHr.vue
+++ src/views/sys/SysHr.vue
@@ -1,15 +1,179 @@
 <template>
     <div>
-        操作员管理
+        <div>
+            <el-input v-model="searchHrName" style="width: 400px;" placeholder="请输入员工名称..." prefix-icon="el-icon-search" @keyup.enter.native="initHrList"></el-input>
+            <el-button icon="el-icon-search" style="margin-left: 10px;" type="primary" @click="initHrList">搜索</el-button>
+        </div>
+        <div class="body-class">
+            <el-card class="card-class" v-for="(hr,index) in hrs">
+                <div slot="header" class="clearfix">
+                    <span>{{hr.name}}</span>
+                    <el-button style="float: right; padding: 3px 0;color:red;" type="text" icon="el-icon-delete" @click="deleteHr(hr)"></el-button>
+                </div>
+                <img :src="hr.userface" :alt="hr.name" :title="hr.name">
+                <div style="margin-top: 10px;">
+                    <table>
+                        <tr>
+                            <td>用户名：</td>
+                            <td>{{hr.name}}</td>
+                        </tr>
+                        <tr>
+                            <td>手机号：</td>
+                            <td>{{hr.telephone}}</td>
+                        </tr>
+                        <tr>
+                            <td>电话号：</td>
+                            <td>{{hr.phone}}</td>
+                        </tr>
+                        <tr>
+                            <td>地址：</td>
+                            <td>{{hr.address}}</td>
+                        </tr>
+                        <tr>
+                            <td>用户状态：</td>
+                            <td>
+                                <el-switch
+                                    v-model="hr.enabled"
+                                    active-color="#13ce66"
+                                    inactive-color="#ff4949"
+                                    @change="updateHr(hr)"
+                                    active-text="启用"
+                                    inactive-text="禁用">
+                                </el-switch>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>用户角色：</td>
+                            <td>
+                                <el-tag class="tag-class" type="success" v-for="(role,indexj) in hr.roles" :key="indexj">{{role.nameZh}}</el-tag>
+                                <el-popover
+                                        placement="right"
+                                        title="用户角色"
+                                        width="200"
+                                        @show="showPopOver(hr)"
+                                        @hide="updateRoles(hr)"
+                                        trigger="click">
+                                    <div>
+                                        <el-select v-model="selectedRoles" placeholder="请选择角色" multiple>
+                                            <el-option
+                                                    v-for="role in roles"
+                                                    :key="role.id"
+                                                    :label="role.nameZh"
+                                                    :value="role.id">
+                                            </el-option>
+                                        </el-select>
+                                    </div>
+                                    <el-button style="margin-left: 4px;" slot="reference" icon="el-icon-more" type="text" @click="initRoles"></el-button>
+                                </el-popover>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>备注：</td>
+                            <td>{{hr.remark}}</td>
+                        </tr>
+                    </table>
+                </div>
+            </el-card>
+        </div>
     </div>
 </template>
 
 <script>
+    import {deleteRequest, getRequest, putRequest} from "../../utils/api";
+
     export default {
-        name: "SysHr"
+        name: "SysHr",
+        mounted() {
+            this.initHrList();
+        },
+        methods:{
+          deleteHr(hr){
+              this.$confirm('此操作将永久删除'+ hr.name +', 是否继续?', '提示', {
+                  confirmButtonText: '确定',
+                  cancelButtonText: '取消',
+                  type: 'warning'
+              }).then(() => {
+                 deleteRequest("/system/hr/deleteHr/"+hr.id).then(resp=>{
+                     if (resp){
+                         this.initHrList();
+                     }
+                 })
+              }).catch(() => {
+                  this.$message({
+                      type: 'info',
+                      message: '已取消删除'
+                  });
+              });
+          },
+          updateRoles(hr){
+              let temp = "?hrid="+hr.id;
+              this.selectedRoles.forEach(role=>{
+                  temp += "&rids=" + role;
+              })
+              putRequest("/system/hr/updateHrRoles/"+temp).then(resp=>{
+                if (resp){
+                    this.initHrList();
+                }
+              })
+          },
+          showPopOver(hr){
+            this.selectedRoles = [];
+            hr.roles.forEach(role=>{
+                this.selectedRoles.push(role.id);
+            })
+          },
+          initRoles(){
+            getRequest("/system/hr/getRoles").then(resp=>{
+                if (resp){
+                    this.roles = resp.object;
+                }
+            })
+          },
+          updateHr(hr){
+              console.log(hr)
+              putRequest("/system/hr/", hr).then(resp=>{
+                  if (resp) {
+                      this.initHrList();
+                  }
+              })
+          },
+          initHrList(){
+              getRequest("/system/hr/?hrName="+this.searchHrName).then(resp=>{
+                  if (resp){
+                      this.hrs = resp.object;
+                  }
+              })
+          }
+        },
+        data(){
+            return {
+                searchHrName: '',
+                hrs: [],
+                roles: [],
+                selectedRoles: [],
+            }
+        }
     }
 </script>
 
-<style scoped>
-
+<style>
+.card-class{
+    width: 400px;
+    height: 450px;
+    margin-top: 15px;
+}
+.body-class{
+    margin-top: 15px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+}
+.body-class img{
+    width: 100px;
+    height: 100px;
+    border-radius: 100px;
+}
+.tag-class{
+    margin-left: 5px;
+}
 </style>
